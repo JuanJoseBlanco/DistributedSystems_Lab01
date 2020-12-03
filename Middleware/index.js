6cbb0a97c96fd6d5d26ea48caf3f43048f176f0f
Juan José Blanco <juan.blanco03@uptc.edu.co>
2020-12-03 02:50:40 -0500

    Servidores Funcionando con Estilos

--- Middleware/index.js
+++ Middleware/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const readLastLines = require('read-last-lines');
-const port = 8000;
+const port = 5000;
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({
+	extended: true
+}));
 
-let serverAStatus = false;
-let serverBStatus = false;
+let serverA = false;
+let serverB = false;
 let time = '';
 
 setInterval(() => {
@@ -13,15 +17,15 @@ setInterval(() => {
 		for (var i = 0; i < data.length; i++) {
 			if (data[i] == '3000') {
 				if (data[i + 1] === '')
-					serverAStatus = 'FAIL';
+					serverA = 'DOWN';
 				else
-					serverAStatus = 'OK';
+					serverA = 'UP';
 				i++;
 			} else if (data[i] == '4000') {
 				if (data[i + 1] === '')
-					serverBStatus = 'FAIL';
+					serverB = 'DOWN';
 				else
-					serverBStatus = 'OK';
+					serverB = 'UP';
 				i++;
 			} else if (data[i].includes('TIME')) {
 				time = data[i];
@@ -32,32 +36,289 @@ setInterval(() => {
 	});
 }, 1000);
 
+
+app.get("/info", (req, res) =>{
+
+	mensaje = {
+		"serverA" : serverA,
+		"serverB" : serverB
+	}
+
+	res.json(mensaje);
+});
+
 app.get("/", (req, res) => {
 	res.send(
-	` <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-	<table class="table">
-    	<thead>
-        	<tr>
-            	<th scope="col">Hora</th>
-            	<th scope="col">Nombre</th>
-            	<th scope="col">Estado</th>
-        	</tr>
-    	</thead>
-    	<tbody>
-        	<tr>
-            	<td rowspan="3">${time}</td>
-        	</tr>
-        	<tr>
-            	<td>Server A</td>
-            	<td>${serverAStatus}</td>
-        	</tr>
-        	<tr>
-            	<td>Server B</td>
-            	<td>${serverBStatus}</td>
-        	</tr>
-    	</tbody>
-	</table> `
+	`<!DOCTYPE html>
+	<html lang="en">
+	
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Monitoring</title>
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+			integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+	</head>
+	
+	<body>
+	
+		<style>
+			body {
+				font: normal 16px/1.5 'Roboto', sans-serif;
+				background: #f1f1f1;
+			}
+	
+			.status {
+				margin-top: 150px;
+			}
+	
+			/* RESET STYLES
+		–––––––––––––––––––––––––––––––––––––––––––––––––– */
+	
+			.chart-skills {
+				margin: 0 auto;
+				padding: 0;
+				list-style-type: none;
+			}
+	
+			.chart-skills *,
+			.chart-skills::before {
+				box-sizing: border-box;
+			}
+	
+	
+			/* CHART-SKILLS STYLES
+		–––––––––––––––––––––––––––––––––––––––––––––––––– */
+	
+			.chart-skills {
+				position: relative;
+				width: 350px;
+				height: 175px;
+				overflow: hidden;
+			}
+	
+			.chart-skills::before,
+			.chart-skills::after {
+				position: absolute;
+			}
+	
+			.chart-skills::before {
+				content: '';
+				width: inherit;
+				height: inherit;
+				border: 45px solid rgba(211, 211, 211, .3);
+				border-bottom: none;
+				border-top-left-radius: 175px;
+				border-top-right-radius: 175px;
+			}
+	
+			.chart-skills::after {
+				content: 'STATUS';
+				left: 50%;
+				bottom: 10px;
+				transform: translateX(-50%);
+				font-size: 1.1rem;
+				font-weight: bold;
+				color: cadetblue;
+			}
+	
+			.chart-skills li {
+				position: absolute;
+				top: 100%;
+				left: 0;
+				width: inherit;
+				height: inherit;
+				border: 45px solid;
+				border-top: none;
+				border-bottom-left-radius: 175px;
+				border-bottom-right-radius: 175px;
+				transform-origin: 50% 0;
+				transform-style: preserve-3d;
+				backface-visibility: hidden;
+				animation-fill-mode: forwards;
+				animation-duration: .4s;
+				animation-timing-function: linear;
+			}
+	
+			.chart-skills li:nth-child(1) {
+				z-index: 2;
+				border-color: firebrick;
+				animation-name: rotate-one;
+			}
+	
+			.chart-skills li:nth-child(2) {
+				z-index: 1;
+				border-color: green;
+				animation-name: rotate-two;
+				animation-delay: .5s;
+			}
+	
+			.chart-skills span {
+				position: absolute;
+				font-size: .85rem;
+				backface-visibility: hidden;
+				animation: fade-in .4s linear forwards;
+			}
+	
+			.chart-skills li:nth-child(1) span {
+				top: 80px;
+				left: 75px;
+				transform: rotate(270deg);
+			}
+	
+			.chart-skills li:nth-child(2) span {
+				top: 20px;
+				left: 10px;
+				transform: rotate(-180deg);
+				animation-delay: .4s;
+			}
+	
+	
+	
+	
+			/* ANIMATIONS
+		–––––––––––––––––––––––––––––––––––––––––––––––––– */
+	
+			@keyframes rotate-one {
+				100% {
+					transform: rotate(90deg);
+					/**
+			 * 12% => 21.6deg
+			 */
+				}
+			}
+	
+			@keyframes rotate-two {
+				0% {
+					transform: rotate(90deg);
+				}
+	
+				100% {
+					transform: rotate(180deg);
+					/**
+			 * 32% => 57.6deg 
+			 * 57.6 + 21.6 => 79.2deg
+			 */
+				}
+			}
+	
+	
+	
+			@keyframes fade-in {
+	
+				0%,
+				90% {
+					opacity: 0;
+				}
+	
+				100% {
+					opacity: 1;
+				}
+			}
+		</style>
+	
+		<nav class="navbar navbar-dark bg-dark">
+			<a class="navbar-brand" href="#">Monitoring</a>
+		</nav>
+	
+		<div class="container">
+			<div class="status">
+				<table class="table">
+					<thead class="thead-dark">
+						<tr>
+							<th scope="col">Name</th>
+							<th scope="col">Time</th>
+							<th scope="col">IP</th>
+							<th scope="col">Status</th>
+							<th scope="col"></th>
+							<th scope="col"></th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<th scope="row">Server1</th>
+							<td>${time}</td>
+							<td>192.168.0.1</td>
+							<td id="serverA">UP</td>
+							<td><span class="badge badge-success" id="serverA-success">Success</span></td>
+							<td><button type="button" class="btn btn-dark">Reboot</button></td>
+						</tr>
+						<tr>
+							<th scope="row">Server2</th>
+							<td>${time}</td>
+							<td>192.168.0.1</td>
+							<td id="serverB">UP</td>
+							<td><span class="badge badge-success" id="serverB-success">Success</span></td>
+							<td><button type="button" class="btn btn-dark">Reboot</button></td>
+						</tr>
+	
+					</tbody>
+				</table>
+			</div>
+	
+	
+			<div class="status">
+				<ul class="chart-skills">
+					<li>
+						<span>Server1</span>
+					</li>
+					<li>
+						<span>Server2</span>
+					</li>
+				</ul>
+			</div>
+		</div>
+	
+		<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+			integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+			crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+			integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MUP7ZeZDyx"
+			crossorigin="anonymous"></script>
+		<script src="https://code.jquery.com/jquery-3.5.1.min.js"
+			integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+		<script>
+			$(document).ready(function () {
+	
+				$.ajax({
+					url: 'http://localhost:5000/info',
+					type: "GET",
+					success: function (res) {
+						if (res.serverA === 'UP') {
+							console.log("lol");
+							$("#serverA").replaceWith("<td id='serverA'>Server OK</td>");
+							$("#serverA-success").replaceWith("<span class='badge badge-success' id='serverA-success'>Success</span>");
+							$(".chart-skills li:nth-child(1)").css("border-color", "green");
+						} else {
+							$("#serverA").replaceWith("<td id='serverA'>Server Caído</td>");
+							$("#serverA-success").replaceWith("<span class='badge badge-danger' id='serverA-success'>DOWN</span>");
+							$(".chart-skills li:nth-child(1)").css("border-color", "firebrick");
+						}
+	
+	
+						if (res.serverB === 'UP') {
+							$("#serverB").replaceWith("<td id='serverB'>Server OK</td>");
+							$("#serverB-success").replaceWith("<span class='badge badge-success' id='serverB-success'>Success</span>");
+							$(".chart-skills li:nth-child(2)").css("border-color", "green");
+						} else {
+							$("#serverB").replaceWith("<td id='serverB'>Server Caído</td>");
+							$("#serverB-success").replaceWith("<span class='badge badge-danger' id='serverB-success'>DOWN</span>");
+							$(".chart-skills li:nth-child(2)").css("border-color", "firebrick");
+						}
+	
+					},
+					error: function () {
+						console.error("No es posible completar la operación");
+					}
+				});
+				$('.btn').click(() => {
+					console.log("Hola");
+				});
+			});
+		</script>
+	</body>
+	
+	</html>`
 	);
 });
 
