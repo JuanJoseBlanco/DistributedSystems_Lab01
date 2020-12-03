b3e3196afebd5f7a815bab44171dd71ad3698a15
Juan José Blanco <juan.blanco03@uptc.edu.co>
2020-12-03 10:04:48 -0500

    Middleware

--- Middleware/monitor.sh
+++ Middleware/monitor.sh
@@ -1,7 +1,3 @@
-#!/bin/bash
-
-<<<<<<< HEAD
-
-=======
+#!/bin
 watch -n 1 "(date '+TIME:%H:%M:%S'; echo 'Server1' ; curl 192.168.5.80:5000; echo '' ;echo 'Server2' ; curl 192.168.5.150:5000 ; echo '') >> log.txt"
->>>>>>> 62b1a81a8353311f1db7919a968c89551d6e515b
+
