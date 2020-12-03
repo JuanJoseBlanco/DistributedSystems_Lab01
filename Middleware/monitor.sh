62b1a81a8353311f1db7919a968c89551d6e515b
Felipe Vargas <felipeesantii@gmail.com>
2020-12-03 09:43:02 -0500

    monitos.sh ip modificada

--- Middleware/monitor.sh
+++ Middleware/monitor.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-watch -n 1 "(date '+TIME:%H:%M:%S'; echo 'Server1' ; curl 192.168.1.188:5000; echo '' ;echo 'Server2' ; curl 192.168.1.168:5000 ; echo '') >> log.txt"
+watch -n 1 "(date '+TIME:%H:%M:%S'; echo 'Server1' ; curl 192.168.1.16:5000; echo '' ;echo 'Server2' ; curl 192.168.1.17:5000 ; echo '') >> log.txt"
